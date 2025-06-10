65c7b9e2a93ba267f6955c5de365b39ddb2a3054
quybao18 <quybao18@gmail.com>
2025-06-10 18:14:10 +0700

    update

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes');
+// const userRoutes = require('./routes/userRoutes');
+// const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -11,8 +11,8 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
+// app.use('/api/users', userRoutes);
+// app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
